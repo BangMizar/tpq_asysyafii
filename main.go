e86cf0481dfa618ac33dce4ec502b0bc2036605d
Azzerith <zikazabat@gmail.com>
2025-08-19 10:28:06 +0700

    add dotenv support for environment variables and update server port configuration

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"tpq_asysyafii/models"
 	"tpq_asysyafii/routes"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	models.ConnectDatabase()
 
 	r := gin.Default()
 
 	routes.UserRoutes(r)
 
-	r.Run(":8080")
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r.Run(":" + port)
 }
