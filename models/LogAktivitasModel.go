ff8e0006f745ba06d58f8e03cdac41636506c468
Azzerith <zikazabat@gmail.com>
2025-08-19 10:09:20 +0700

    add LogAktivitas model with fields and relationships

---
+++ models/LogAktivitasModel.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type LogAktivitas struct {
+	IDLog     string    `json:"id_log" gorm:"type:char(36);primaryKey"`
+	IDUser    string    `json:"id_user" gorm:"type:char(36);not null"`
+	Aksi      string    `json:"aksi" gorm:"type:varchar(100);not null"`
+	TipeTarget string   `json:"tipe_target" gorm:"type:varchar(50)"`
+	IDTarget  string    `json:"id_target" gorm:"type:char(36)"`
+	Keterangan string   `json:"keterangan" gorm:"type:text"`
+	WaktuAksi time.Time `json:"waktu_aksi" gorm:"autoCreateTime"`
+
+	User User `json:"user" gorm:"foreignKey:IDUser;references:IDUser"`
+}
