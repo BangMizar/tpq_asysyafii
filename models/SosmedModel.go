9619b7689101be8b2cbb0b32eb9f7c42c213db97
Azzerith <zikazabat@gmail.com>
2025-10-20 21:40:51 +0700

    add sosmed model

---
+++ models/SosmedModel.go
@@ -0,0 +1,18 @@
+package models
+
+import "time"
+
+type SosialMedia struct {
+	IDSosmed       string    `json:"id_sosmed" gorm:"column:id_sosmed;primaryKey;type:char(36)"`
+	NamaSosmed     string    `json:"nama_sosmed" gorm:"type:varchar(100);not null"` // Contoh: "Facebook", "Instagram", "YouTube"
+	LinkSosmed     *string   `json:"link_sosmed,omitempty" gorm:"type:varchar(500)"`
+	DiupdateOlehID *string   `json:"diupdate_oleh_id,omitempty" gorm:"column:diupdate_oleh_id;type:char(36)"`
+	DibuatPada     time.Time `json:"dibuat_pada" gorm:"autoCreateTime"`
+	DiperbaruiPada time.Time `json:"diperbarui_pada" gorm:"autoUpdateTime"`
+	
+	DiupdateOleh *User `json:"diupdate_oleh,omitempty" gorm:"foreignKey:DiupdateOlehID;references:IDUser"`
+}
+
+func (SosialMedia) TableName() string {
+	return "sosial_media"
+}
